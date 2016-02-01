3f956b6be0b14d1b7b003b113cab047555f11d26
Steven Gantz <steven.p.gantz@gmail.com>
2016-02-01 14:42:47 -0500

    Create MemoryCard.java
    Started writing the memory card class

---
+++ MemoryCardGame/MemoryCard.java
@@ -0,0 +1,35 @@
+package com.stevengantz.memory;
+
+/**
+ * Author: Steven Gantz
+ * Date: 2/1/2016
+ * Due:
+ * Dr. Spiegel, CSC-421
+ * This file is a container representing a card in memory
+ *  that is used to play a card game called Memory.
+ **/
+public class MemoryCard {
+  
+  /**
+   * This attribute contains the symbol that
+   * would show on the front of a memory card.
+   **/
+  private char cardFront;
+  
+  /**
+   * This attribute contains the symbol that
+   * would show on the back of a memory card.
+   **/
+  private char cardBack;
+  
+  /**
+   * This value denotes whether the cards current
+   * state is upward or downward facing. 
+   * true - Upward facing
+   * false - Downward facing
+   **/
+  private boolean faceUp;
+  
+  
+  
+}
