7223a2e4ec99e23c8beae6ca57ab13e647bc42b8
Steve <steven.p.gantz@gmail.com>
2016-03-06 19:39:06 -0500

    Made some more changes

---
+++ Project2/src/stevengantz/memory/player/MemoryPlayer.java
@@ -0,0 +1,13 @@
+package stevengantz.memory.player;
+
+/**
+ * @author Steven Gantz
+ * @date 3/6/2016
+ * @file This abstract class implements all methods and fields that extending
+ *       classes must use in order to be considered MemoryGamePlayers.
+ *       MemoryPlayer implements the Player interface which contains the
+ *       contract for all players.
+ */
+public abstract class MemoryPlayer implements Player{
+
+}
